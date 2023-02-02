0c8eb433f998becbb7ce04a3697fd6598b93a505
patkkh <patkkh@gmail.com>
2023-02-02 21:51:12 +0900

    Delete build directory

--- build/ceilometer/ceilometer_key.py
+++
@@ -1,8 +0,0 @@
-import xarray as xr
-import pandas as pd
-
-fn = 'A2082800.nc'
-ds = xr.open_dataset(fn)
-values = list(ds.keys())
-with open("ceilometer_keys.txt", "w") as output:
-    output.write(str(values))
